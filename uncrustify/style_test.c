31ad6740adf216bc9328ad8c80652f41fd80bffb
lf <francuccilea@gmail.com>
2019-03-19 23:51:04 -0300

    Added static code analyzer, ceedling, among other tools

---
+++ uncrustify/style_test.c
@@ -0,0 +1,82 @@
+/**
+ */
+
+#define LOLO(a, b)      (a##b)
+#define TOTO(a, b)      #b
+
+void main(void)
+{
+
+
+    int i;
+    int **pointer_to_pointer;
+
+    for(i = 0, i = 0, i = 0, i = 0, i = 0, i = 0, i = 0;
+        i < 10;
+        ++i, ++i, ++i, ++i, ++i)
+    {
+#if YY==0
+        printf(  "yy"         );
+#else
+        printf( "yy");
+#endif
+        printf();
+
+
+
+    }
+
+    /*
+     *  hello
+     */
+
+    ++(p)->lolo = --(p->toto);
+    ++(p)->hinfo.rcvevt;
+    #define INFO_RCV_EVENTS(p)      ++(p)->hinfo.rcvevt
+    #define INFO_RCV_EVENTS(p) \
+        ++(p)->hinfo.rcvevt
+
+
+
+    if(i==0&&j==1)
+        return;
+}
+
+
+
+/*  Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+  bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla */
+
+// hello hello hello hello hello hello hello 
+//hello hello hello hello hello 
+
+void thisFunction(unsigned long veryLargeeeeeeeeeeeeeeeeeeeeeeeeeeeeee,
+                  unsigned long veryLaaaaaaaaaaaaaaaaaarge)
+{
+}
+
+static int is_coco(   void  );
+static int    is_lolo(   void  ) { int i; }
+
+#define      YY     lolo++               \
+    hello1();           \
+    hello2();   \
+    bye();
+
+#define SOSO            toto++, ++lolo
+    
+#ifdef COCO
+    #if COCO
+        #define YY                                      \
+            hello1();           \
+            hello2();   \
+            bye();
+    #else
+        #define YY          \
+            hello
+    #endif
+#elif LOLO
+#define YY  1
+#else
+#define YY  2
+#endif
